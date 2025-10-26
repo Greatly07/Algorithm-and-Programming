e8db76680dcbb74773f388bcde3bf9ab28a16295
Greatly <greatly07@gmail.com>
2025-10-26 09:43:34 +0700

    added ae fundamentals

---
+++ AlgoProg/Online_Judge/0_AE_Fundamental/Late_Night_Counting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d\n", n);
+        n++;
+    }
+
+    return 0;
+}
